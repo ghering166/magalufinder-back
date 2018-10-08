3b4d85fc72157eae1c28193e6ccc40e48e994171
Henrique Santiago <henriquesantiagofranco@gmail.com>
2018-10-08 17:35:04 -0300

    Ajustes query para busca de produtos

--- src/main/java/com/magalufinder/repository/StoreRepository.java
+++ src/main/java/com/magalufinder/repository/StoreRepository.java
@@ -12,7 +12,8 @@ import com.magalufinder.util.ProductStoreTO;
 public interface StoreRepository extends CrudRepository<Store, Long>{
 
 	
-	 @Query(value = " SELECT * FROM tab_product p\r\n" + 
+	
+	@Query(value = " SELECT * FROM tab_product p\r\n" + 
 		 		" INNER JOIN tab_product_stores ps ON ps.id_product = p.id\r\n" + 
 		 		" INNER JOIN tab_stores s ON s.id = ps.id_store\r\n" + 
 		 		" WHERE ps.id_store = :id", nativeQuery = true)
